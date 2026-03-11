cda275e71ea6b27f1592038c5e8ce70ef9c28eb3
Emir Toker <emirtoker@Emirs-MacBook-Pro.local>
2026-03-11 16:14:01 +0300

    test: running env check inside main.py

--- app/main.py
+++ app/main.py
@@ -62,7 +62,7 @@ if st.session_state.get('synthesis_active') and multi_bundle[0]: show_map = True
 if show_map:
     # Milimetrik GeoTIFF Engine devrede
     m = create_interactive_map(layers_data, shp, one_bundle, multi_bundle, units_data, av_dict)
-    m.to_streamlit(height=850, key=f"map_render_{time.time()}")
+    m.to_streamlit(height=1200, key=f"map_render_{time.time()}")
 else:
     # Boş harita hali
     m = leafmap.Map(center=[39, 35], zoom=6, tiles=None, control_scale=True, zoom_snap=0.1, zoom_delta=0.1)
